8a95e8796bafac0bfa15bddf12875c5ff3622285
Daan Krol <daankrol06@gmail.com>
2025-10-08 11:09:54 +0200

    Database ORM/sql-lite

---
+++ WebApplication1/PlantLiefhebbersContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+public class PlantLiefhebbersContext : DbContext
+{
+    public DbSet<Klant> klant { get; set; }
+    public DbSet<Product> product { get; set; }
+
+    public string DbPath { get; }
+
+    public PlantLiefhebbersContext()
+    {
+        //var folder = Environment.SpecialFolder.LocalApplicationData;
+        //var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(Environment.CurrentDirectory, "plantliefebbersontext.db");
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
+public class Klant {
+    public int klantId { get; set; }
+    public string naam { get; set; }
+
+}
+
+public class Product {
+    public int productId { get; set; }
+    public string naam { get; set; }
+    public string beschrijving { get; set; }
+    public decimal prijs { get; set; }
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,5 +1,37 @@
-
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Linq;
+using System.Reflection.Metadata;
 using Microsoft.OpenApi.Models;
+
+using var db = new PlantLiefhebbersContext();
+
+//// Note: This sample requires the database to be created before running.
+//Console.WriteLine($"Database path: {db.DbPath}.");
+
+// Create
+Console.WriteLine("Inserting a new blog");
+db.Add(new Klant { klantId = 44321, naam = "john" });
+await db.SaveChangesAsync();
+
+//// Read
+//Console.WriteLine("Querying for a blog");
+//var blog = await db.Blogs
+//    .OrderBy(b => b.BlogId)
+//    .FirstAsync();
+
+//// Update
+//Console.WriteLine("Updating the blog and adding a post");
+//blog.Url = "";
+//blog.Posts.Add(
+//    new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+//await db.SaveChangesAsync();
+
+//// Delete
+//Console.WriteLine("Delete the blog");
+//db.Remove(blog);
+//await db.SaveChangesAsync();
 
 namespace WebApplication1
 {
@@ -7,7 +39,6 @@ namespace WebApplication1
     {
         public static void Main(string[] args)
         {
-
             Console.WriteLine("Hello, World!");
 
             var builder = WebApplication.CreateBuilder(args);
@@ -18,7 +49,7 @@ namespace WebApplication1
 
             // Add Swagger services
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(c => {c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" } ); } );
+            builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" }); });
 
             var app = builder.Build();
             // Configure the HTTP request pipeline.
@@ -31,31 +62,9 @@ namespace WebApplication1
                 });
             }
 
-            // Add services to the container.
-            //builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            //builder.Services.AddOpenApi();
-
-            //var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            //if (app.Environment.IsDevelopment())
-            //{
-            //    app.MapOpenApi();
-            //}
-
-            
             app.UseRouting();
-            
-           
-
             app.UseHttpsRedirection();
-
-            //app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.Run();
         }
     }
