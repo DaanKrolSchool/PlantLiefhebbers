4d7c21bc6207c49ceb8041bb51d4886f3f9d671e
Daan Krol <daankrol06@gmail.com>
2025-10-08 23:36:48 +0200

    swagger werkt nu

--- WebApplication1/Controllers/KlantTestController.cs
+++ WebApplication1/Controllers/KlantTestController.cs
@@ -15,16 +15,77 @@ namespace WebApplication1.Controllers
         }
 
         /// <summary>
-        /// Voeg een nieuwe klant toe aan de database.
+        /// Haal alle klanten op.
         /// </summary>
-        /// <param name="klant">De klant die toegevoegd moet worden.</param>
-        /// <returns>De toegevoegde klant.</returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Klant>>> GetKlanten()
+        {
+            return await _context.klant.ToListAsync();
+        }
+
+        /// <summary>
+        /// Haal een specifieke klant op.
+        /// </summary>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Klant>> GetKlant(int id)
+        {
+            var klant = await _context.klant.FindAsync(id);
+            if (klant == null)
+                return NotFound();
+            return klant;
+        }
+
+        /// <summary>
+        /// Voeg een nieuwe klant toe.
+        /// </summary>
         [HttpPost]
         public async Task<ActionResult<Klant>> PostKlant([FromBody] Klant klant)
         {
             _context.klant.Add(klant);
             await _context.SaveChangesAsync();
-            return CreatedAtAction(nameof(PostKlant), new { id = klant.klantId }, klant);
+            return CreatedAtAction(nameof(GetKlant), new { id = klant.klantId }, klant);
+        }
+
+        /// <summary>
+        /// Pas een bestaande klant aan.
+        /// </summary>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutKlant(int id, [FromBody] Klant klant)
+        {
+            if (id != klant.klantId)
+                return BadRequest();
+
+            _context.Entry(klant).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.klant.Any(e => e.klantId == id))
+                    return NotFound();
+                else
+                    throw;
+            }
+
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Verwijder een klant.
+        /// </summary>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteKlant(int id)
+        {
+            var klant = await _context.klant.FindAsync(id);
+            if (klant == null)
+                return NotFound();
+
+            _context.klant.Remove(klant);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
