3da2c35ff697aea471b56d025a24ce4e13f11495
Hugo0000000 <24087823@student.hhs.nl>
2025-10-29 11:55:34 +0100

    Routing gefix/inlog pagina

--- WebApplication1/ClientApp/src/App.jsx
+++ WebApplication1/ClientApp/src/App.jsx
@@ -1,45 +1,32 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import './App.css';
 import ProductAanmelden from "./ProductAanmelden.jsx";
+import InlogScherm from "./InlogScherm.jsx";
 
-function App() {
+function Home() {
     const navigate = useNavigate();
-    
+
     return (
         <div className="App-background">
             <h1>Bied op planten</h1>
             <p>De plek om planten te kopen en verkopen,</p>
-
-            <button
-                className="OverzichtButton"
-                onClick={() => navigate("/ProductAanmelden")}>Bekijk onze planten</button>
-            <switch>
-                <Routes>
-                    <Route path="/ProductAanmelden" element={<ProductAanmelden />} />
-                </Routes>
-            </switch>
-
-            <button
-                className="AanvoerderButton"
-                onClick={() => navigate("/ProductAanmelden")}>Aanvoerder Overzicht</button>
-            <switch>
-                <Routes>
-                    <Route path="/ProductAanmelden" element={<ProductAanmelden />} />
-                </Routes>
-            </switch>
-
-            <button
-                className="LoginButton"
-                onClick={() => navigate("/ProductAanmelden")}>Inloggen</button>
-            <switch>
-                <Routes>
-                    <Route path="/ProductAanmelden" element={<ProductAanmelden />} />
-                </Routes>
-
-            </switch>
+            <button className="OverzichtButton" onClick={() => navigate("/ProductAanmelden")}>Bekijk onze planten</button>
+            <button className="AanvoerderButton" onClick={() => navigate("/ProductAanmelden")}>Aanvoerder overzicht</button>
+            <button className="LoginButton" onClick={() => navigate("/InlogScherm")}>Inloggen</button>
         </div>
     );
 }
+
+function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/ProductAanmelden" element={<ProductAanmelden />} />
+            <Route path="/InlogScherm" element={<InlogScherm />} />
+
+        </Routes>
+    );
+}
+
 export default App;
---
+++ WebApplication1/ClientApp/src/InlogScherm.jsx
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+import './InlogScherm.css';
+
+function InlogScherm() {
+    return (
+        <div classnName="content">
+            <h1>Login</h1>
+            <p>Type hier je gebruikersnaam en wachtwoord in</p>
+
+            <form classnName="inputVak">
+                <label htmlFor="email">Email:</label>
+                <input type="text" name="email" /><br />
+                <label htmlFor="wachtwoord">Wachtwoord:</label>
+                <input type="text" name="wachtwoord" /><br />
+            </form>
+        </div>
+    );
+}
+
+export default InlogScherm;
