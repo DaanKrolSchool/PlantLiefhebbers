662521a359d27339895e90463f01d85bbe30820e
Hugo0000000 <24087823@student.hhs.nl>
2025-10-29 21:02:45 +0100

    InlogScherm toegevoegd

--- WebApplication1/ClientApp/src/InlogScherm.jsx
+++ WebApplication1/ClientApp/src/InlogScherm.jsx
@@ -1,18 +1,48 @@
-﻿import React from 'react';
+﻿import React, { useState } from "react";
 import './InlogScherm.css';
+import { Routes, Route, useNavigate, BrowserRouter } from "react-router-dom";
 
 function InlogScherm() {
+    const [Email, setEmail] = useState("");
+    const [Password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    function CheckInlog(e) {
+        e.preventDefault()
+        if (Email === "Pedro@mail" && Password === "Pedro") {
+            alert("Hoerey!! Succes!")
+            navigate("/");
+        }
+        else { alert("Goed geprobeert, maar dat klopt niet.") }
+
+    }
     return (
-        <div classnName="content">
+        <div className="content">
             <h1>Login</h1>
-            <p>Type hier je gebruikersnaam en wachtwoord in</p>
+            <p>Type hier je e-mailadres en wachtwoord in</p>
 
             <form classnName="inputVak">
-                <label htmlFor="email">Email:</label>
-                <input type="text" name="email" /><br />
-                <label htmlFor="wachtwoord">Wachtwoord:</label>
-                <input type="text" name="wachtwoord" /><br />
+                <input
+                    id="email"
+                    type="text" name="email"
+                    value={Email}
+                    placeholder="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <br />
+                <input
+                    id="pssword"
+                    type="password" name="password"
+                    value={Password}
+                    placeholder="Wachtwoord"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <br />
             </form>
+
+            <button className="CheckInlogButton" onClick={CheckInlog}>Verder</button>
+            <button className="Back" type="button" onClick={() => navigate("/")}>Terug</button>
+
         </div>
     );
 }
