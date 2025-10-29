b4193a043f3aec13d0e77186bdd053f3c7bccb00
Daan Krol <daankrol06@gmail.com>
2025-10-29 10:35:12 +0100

    App/Hoofdpagina af

--- WebApplication1/ClientApp/src/App.jsx
+++ WebApplication1/ClientApp/src/App.jsx
@@ -11,11 +11,33 @@ function App() {
         <div className="App-background">
             <h1>Bied op planten</h1>
             <p>De plek om planten te kopen en verkopen,</p>
-            <button onClick={() => navigate("/ProductAanmelden")}>Aanvoerder Overzicht</button>
+
+            <button
+                className="OverzichtButton"
+                onClick={() => navigate("/ProductAanmelden")}>Bekijk onze planten</button>
             <switch>
                 <Routes>
                     <Route path="/ProductAanmelden" element={<ProductAanmelden />} />
                 </Routes>
+            </switch>
+
+            <button
+                className="AanvoerderButton"
+                onClick={() => navigate("/ProductAanmelden")}>Aanvoerder Overzicht</button>
+            <switch>
+                <Routes>
+                    <Route path="/ProductAanmelden" element={<ProductAanmelden />} />
+                </Routes>
+            </switch>
+
+            <button
+                className="LoginButton"
+                onClick={() => navigate("/ProductAanmelden")}>Inloggen</button>
+            <switch>
+                <Routes>
+                    <Route path="/ProductAanmelden" element={<ProductAanmelden />} />
+                </Routes>
+
             </switch>
         </div>
     );
