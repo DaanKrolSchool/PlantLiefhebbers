729c42b8d78b8613695819e77b1cb442acff1d5b
JustPott1 <justin.roos.yologames@gmail.com>
2025-12-11 14:50:16 +0100

    Registertest geupdate met meer tests en comments.

--- TestProjectPlantliefhebbers/Registertest.cs
+++ TestProjectPlantliefhebbers/Registertest.cs
@@ -4,91 +4,181 @@ using Moq;
 using System.Threading.Tasks;
 using WebApplication1.Controllers;
 
-public class RegisterTest
+
+namespace WebApiTests
 {
-    private Mock<UserManager<User>> GetMockUserManager()
-    {
-        var store = new Mock<IUserStore<User>>();
-        return new Mock<UserManager<User>>(store.Object, null, null, null, null, null, null, null, null);
-    }
-
-    private Mock<RoleManager<IdentityRole>> GetMockRoleManager()
-    {
-        var store = new Mock<IRoleStore<IdentityRole>>();
-        return new Mock<RoleManager<IdentityRole>>(store.Object, null, null, null, null);
-    }
-
-    [Fact]
-    public async Task MissingName()
-    {
-        var userManager = GetMockUserManager();
-        var roleManager = GetMockRoleManager();
-        var controller = new KlantRegisterController(userManager.Object, roleManager.Object);
-
-        var dto = new KlantRegisterDto
+    public class RegisterTest
+    { 
+        //Mock usermanager aanmaken omdat de echte usermanager met de database werkt
+        private Mock<UserManager<User>> GetMockUserManager()
         {
-            naam = "",
-            email = "test@mail.com",
-            wachtwoord = "123456"
-        };
-
-        var result = await controller.Register(dto);
-
-        Assert.IsType<BadRequestObjectResult>(result);
-    }
-
-    [Fact]
-    public async Task ExistingEmail()
-    {
-        var userManager = GetMockUserManager();
-        var roleManager = GetMockRoleManager();
-
-        // Pretend user exists
-        userManager.Setup(x => x.FindByEmailAsync("test@mail.com"))
-                   .ReturnsAsync(new User());
-
-        var controller = new KlantRegisterController(userManager.Object, roleManager.Object);
-
-        var dto = new KlantRegisterDto
+            var store = new Mock<IUserStore<User>>(); 
+            return new Mock<UserManager<User>>(store.Object, null, null, null, null, null, null, null, null);
+        }
+        
+        //Mock rolemanager aanmaken
+        private Mock<RoleManager<IdentityRole>> GetMockRoleManager()
         {
-            naam = "Justin",
-            email = "test@mail.com",
-            wachtwoord = "123456"
-        };
+            var store = new Mock<IRoleStore<IdentityRole>>();
+            return new Mock<RoleManager<IdentityRole>>(store.Object, null, null, null, null);
+        }
 
-        var result = await controller.Register(dto);
-
-        Assert.IsType<BadRequestObjectResult>(result);
-    }
-
-    [Fact]
-    public async Task ValidData()
-    {
-        var userManager = GetMockUserManager();
-        var roleManager = GetMockRoleManager();
-
-        // Pretend no user exists
-        userManager.Setup(x => x.FindByEmailAsync(It.IsAny<string>()))
-                   .ReturnsAsync((User)null);
-
-        userManager.Setup(x => x.FindByNameAsync(It.IsAny<string>()))
-                   .ReturnsAsync((User)null);
-
-        // Pretend create succeeds
-        userManager.Setup(x => x.CreateAsync(It.IsAny<User>(), It.IsAny<string>()))
-                   .ReturnsAsync(IdentityResult.Success);
-
-        var controller = new KlantRegisterController(userManager.Object, roleManager.Object);
-
-        var dto = new KlantRegisterDto
+        [Fact]
+        public async Task NoName()
         {
-            naam = "Test",
-            email = "Test@gmail.com",
-            wachtwoord = "Test1!"
-        };
+            // maakt een controller aan met de mock UserManager en RoleManager (arrange)
+            var userManager = GetMockUserManager();
+            var roleManager = GetMockRoleManager();
+            var controller = new KlantRegisterController(userManager.Object, roleManager.Object);
 
-        var result = await controller.Register(dto);
+            //DTO zonder naam, hoort fout te gaan
+            var dto = new KlantRegisterDto
+            {
+                naam = "",
+                email = "Test@gmail.com",
+                wachtwoord = "Test1!"
+            }; 
 
-        Assert.IsType<OkObjectResult>(result);
+            //Act
+            var result = await controller.Register(dto);
+
+            //controller hoort badrequest terug te geven want geen naam (assert)
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task NoEmail()
+        {
+            // maakt een controller aan met de mock UserManager en RoleManager (arrange)
+            var userManager = GetMockUserManager();
+            var roleManager = GetMockRoleManager();
+            var controller = new KlantRegisterController(userManager.Object, roleManager.Object);
+
+            //DTO zonder email, hoort fout te gaan
+            var dto = new KlantRegisterDto
+            {
+                naam = "Test",
+                email = "",
+                wachtwoord = "Test1!"
+            };
+
+            //Act
+            var result = await controller.Register(dto);
+
+            //controller hoort badrequest terug te geven want geen email (assert)
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task ExistingEmail()
+        {
+            //Arrange
+            var userManager = GetMockUserManager();
+            var roleManager = GetMockRoleManager();
+
+            //email zit al in het systeem
+            userManager.Setup(x => x.FindByEmailAsync("Test@gmail.com"))
+                       .ReturnsAsync(new User());
+
+            var controller = new KlantRegisterController(userManager.Object, roleManager.Object);
+            //DTO met bestaande email
+            var dto = new KlantRegisterDto
+            {
+                naam = "Test",
+                email = "Test@gmail.com",
+                wachtwoord = "Test1!"
+            };
+
+            //Act
+            var result = await controller.Register(dto);
+
+            //controller hoort badrequest terug te geven want email bestaat al (assert)
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task ExistingNaam()
+        {
+            //Arrange
+            var userManager = GetMockUserManager();
+            var roleManager = GetMockRoleManager();
+
+            //naam zit al in het systeem
+            userManager.Setup(x => x.FindByNameAsync("Test"))
+                       .ReturnsAsync(new User());
+
+            var controller = new KlantRegisterController(userManager.Object, roleManager.Object);
+            //DTO met bestaande naam
+            var dto = new KlantRegisterDto
+            {
+                naam = "Test",
+                email = "Test@gmail.com",
+                wachtwoord = "Test1!"
+            };
+
+            //Act
+            var result = await controller.Register(dto);
+
+            //controller hoort badrequest terug te geven want naam bestaat al (assert)
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task ValidInput()
+        {
+            //Arrange
+            var userManager = GetMockUserManager();
+            var roleManager = GetMockRoleManager();
+
+            // Geen email of naam bestaat.
+            userManager.Setup(x => x.FindByEmailAsync(It.IsAny<string>()))
+                       .ReturnsAsync((User)null);
+
+            userManager.Setup(x => x.FindByNameAsync(It.IsAny<string>()))
+                       .ReturnsAsync((User)null);
+
+            // UserManager geslaagd, gebruiker is gemaakt
+            userManager.Setup(x => x.CreateAsync(It.IsAny<User>(), It.IsAny<string>()))
+                       .ReturnsAsync(IdentityResult.Success);
+
+            var controller = new KlantRegisterController(userManager.Object, roleManager.Object);
+
+            var dto = new KlantRegisterDto
+            {
+                naam = "Test",
+                email = "Test@gmail.com",
+                wachtwoord = "Test1!"
+            };
+
+            //Act
+            var result = await controller.Register(dto);
+
+            //controller hoort een okresult terug te geven want alles is goed ingevuld en het bestaat nog niet. (assert)
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task PasswordTest()
+        {
+            // Arrange
+            var userManager = GetMockUserManager();
+            var roleManager = GetMockRoleManager();
+
+            var controller = new KlantRegisterController(userManager.Object, roleManager.Object);
+
+            // DTO met fout wachtwoord
+            var dto = new KlantRegisterDto
+            {
+                naam = "Test",
+                email = "Test@gmail.com",
+                wachtwoord = "123"
+            };
+
+            //Act
+            var result = await controller.Register(dto);
+
+            //controller hoort badrequest te geven want wachtwoord voldoet niet aan de eisen (assert)
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
     }
 }
