be31b389de5611de21fac2162cac044eae67de44
FJSeverinH <severin.heidecke@gmail.com>
2026-01-13 11:53:22 +0100

    database en context update 1

--- WebApplication1/PlantLiefhebbersContext.cs
+++ WebApplication1/PlantLiefhebbersContext.cs
@@ -10,23 +10,9 @@ public class PlantLiefhebbersContext : IdentityDbContext<User>
         : base(options)
     {
     }
-
-    public DbSet<Klant> klant { get; set; }
     public DbSet<Product> product { get; set; }
     public DbSet<Veiling> veiling { get; set; }
-    public DbSet<Aanvoerder> aanvoerder { get; set; }
-    public DbSet<Veilingmeester> veilingmeester { get; set; }
     public DbSet<ProductVerkoopHistorie> productVerkoopHistorie { get; set; }
-
-}
-
-
-public class Klant {
-    public int klantId { get; set; }
-    public string naam { get; set; }
-    public string adres { get; set; }
-    public string email { get; set; }
-    public string wachtwoord { get; set; }
 
 }
 
@@ -52,7 +38,7 @@ public class Product {
     public TimeSpan? veilTijd { get; set; }
     public string aanvoerderId { get; set; }
 	public int positie { get; set; }
-
+    public float prijs { get; set; }
     public bool isVerkocht { get; set; } = false;
     public float? verkoopPrijs { get; set; }
     public DateTime? verkoopDatum { get; set; }
@@ -66,26 +52,6 @@ public class Veiling
     public float startPrijs { get; set; }
     public string startDatum { get; set; }
     public string klokLocatie { get; set; }
-
-}
-
-public class Aanvoerder
-{
-    public int aanvoerderId { get; set; }
-    public string naam { get; set; }
-    public string adres { get; set; }
-    public string email { get; set; }
-    public string wachtwoord { get; set; }
-
-}
-
-public class Veilingmeester
-{
-    public int veilingmeesterId { get; set; }
-    public string naam { get; set; }
-    public string adres { get; set; }
-    public string email { get; set; }
-    public string wachtwoord { get; set; }
 
 }
 
