0ce4676a159f5d59feb2a6b27ad5f756916bfff7
FJSeverinH <severin.heidecke@gmail.com>
2026-01-21 00:52:57 +0100

    accessibility

--- WebApplication1/ClientApp/src/Veilingmeester/VerkoopOverzicht.tsx
+++ WebApplication1/ClientApp/src/Veilingmeester/VerkoopOverzicht.tsx
@@ -58,8 +58,8 @@ function VerkoopOverzichtVeilingmeester() {
                                 <h3>{p.soortPlant}</h3>
                                 <p>Aantal verkocht: {p.aantalVerkocht}</p>
                                 <p>Prijs per stuk: {p.prijsPerStuk.toFixed(2)}</p>
-                                <p style={{ opacity: 0.7 }}>Naam aanvoerder: {p.aanvoerderNaam}</p>
-                                <p style={{ opacity: 0.7 }}>Naam klant: {p.klantNaam}</p>
+                                <p>Naam aanvoerder: {p.aanvoerderNaam}</p>
+                                <p>Naam klant: {p.klantNaam}</p>
                             </div>
                         ))}
                     </div>
